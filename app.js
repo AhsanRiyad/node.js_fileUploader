0bd104768476c01dc401b47a74f31ca01d56b07d
AhsanRiyad <riyad298@gmail.com>
2021-03-03 13:16:48 +0600

    completed

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-
-
 const express = require('express');
 const multer = require('multer');
 const ejs = require('ejs');
 const path = require('path');
 const uuid = require('uuid');
-
-
-
-
+let fs = require('fs-extra');
 
 // Set The Storage Engine
 const storage = multer.diskStorage({
-    destination: './public/uploads/',
+    destination: (req, file, callback) => {
+        let path = 
+        req.url == 'image_upload' ? `./public/uploads/image` :
+        req.url == 'zip_upload' ? `./public/uploads/zip` :
+        req.url == 'video_upload' ? `./public/uploads/video` :
+        '';
+        fs.mkdirsSync(path);
+        callback(null, path);
+    },
     filename: function (req, file, cb) {
         // cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
         cb(null, file.fieldname + '-' + uuid.v4() + path.extname(file.originalname));
@@ -22,16 +25,36 @@ const storage = multer.diskStorage({
 // Init Upload
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 10000000000000 },
+    limits: { fileSize: 10000000000000000000000000 },
     fileFilter: function (req, file, cb) {
-        checkFileType(file, cb);
+        checkFileType(req, file, cb);
     }
-}).single('myImage');
+}).single('file');
 
 // Check File Type
-function checkFileType(file, cb) {
+function checkFileType(req, file, cb) {
     // Allowed ext
-    const filetypes = /jpeg|jpg|png|gif/;
+    let filetypes = 
+    req.url == '/image_upload' ? /jpeg|jpg|png|gif/ :
+    req.url == '/zip_upload' ? /zip/ :
+    req.url == '/video_upload' ? /mp4|mkv|mov|wmv|flv|avi|webm|mkv/ : 
+    ''  ;
+
+    /* switch (req.url) {
+        case '/image_upload':
+            filetypes == /jpeg|jpg|png|gif/
+            break;
+        case '/zip_upload':
+            filetypes == /zip/
+            break;
+        case '/video_upload':
+            filetypes == /mkv/
+            break;
+        default:
+            filetypes == /jpeg|jpg|png|gif/
+            break;
+    } */
+
     // Check ext
     const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
     // Check mime
@@ -47,7 +70,6 @@ function checkFileType(file, cb) {
 // Init app
 const app = express();
 
-
 // Allow cross origin resource sharing (CORS) within our application
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -59,14 +81,17 @@ app.use(function (req, res, next) {
 app.set('view engine', 'ejs');
 
 // Public Folder
-app.use(express.static('./public'));
+// app.use(express.static('./public'));
+app.use('/p', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => res.render('index'));
 
-app.post('/upload', (req, res) => {
+app.post(['/image_upload' , '/zip_upload' , '/video_upload'], (req, res) => {
+    console.log(req.url);
+
     upload(req, res, (err) => {
         if (err) {
-            console.log('if err' ,  err);
+            // console.log('if err' ,  err);
             res.send(err.code);
         } else {
             if (req.file == undefined) {
